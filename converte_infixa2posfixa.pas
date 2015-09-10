d211224c25337935022ab334f94400d2db251a1e
anderxy <anderson.goma@gmail.com>
2015-09-10 00:09:34 -0300

    Converte de infixa completamente parentesiada para posfixa

---
+++ converte_infixa2posfixa.pas
@@ -0,0 +1,30 @@
+{Conversao de infixa completamente 
+parentesiada para posfixa}
+program converte;
+uses crt, pilhas;
+var e: string;
+
+function posfixa(e: string): string;
+var i: integer;
+	s: string;
+	p: pilha;
+begin
+	s:='';
+	iniciap(p);
+	for i:= 1 to length(e) do
+		case e[i] of
+		'0'..'9'       : s:=s+e[i];
+		'+','-','*','/': empilha(e[i],p);
+		')'            : s:=s+desempilha(p);
+		end;
+	posfixa := s;
+end;
+
+BEGIN
+	clrscr;
+	write('infixa?');
+	readln(e);
+	writeln('Posfixa:', posfixa(e));
+	readln;
+END.
+
