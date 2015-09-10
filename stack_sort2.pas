ce11dbcb2569ff38e02242fec53cb31c42b5ee2c
anderxy <anderson.goma@gmail.com>
2015-09-09 23:29:46 -0300

    Stack_sort ordenando decrescente

---
+++ stack_sort2.pas
@@ -0,0 +1,27 @@
+Program stack_sort2;
+uses crt, pilhas;
+var n,i: integer;
+    x: real;
+    A,B: pilha;
+
+BEGIN
+    clrscr;
+    write('Qtde?');
+    readln(n);
+    iniciap(A);
+    iniciap(B);
+    
+    for i:=1 to n do
+      begin
+        write(i, '.o núm?');
+        readln(x);
+        while not vaziap(A) and (x <= topo(A)) do
+            empilha(desempilha(A), B);
+        empilha(x,A);
+        while not vaziap(B) do empilha(desempilha(B),A);      
+      end;
+
+    write('Sequencia ordenada:');
+    while not vaziap(A) do writeln(desempilha(A));
+    readln;
+END.
